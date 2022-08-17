90e039172b5f8e841e7da7d7a1dc8d77d301b6ea
Morris Richman <mcrich23@icloud.com>
2022-08-16 19:57:46 -0700

    Initial Commit

---
+++ Seattle Bridge Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Seattle Bridge Tracker
+//
+//  Created by Morris Richman on 8/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Seattle Bridge Tracker/Seattle_Bridge_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seattle_Bridge_TrackerApp.swift
+//  Seattle Bridge Tracker
+//
+//  Created by Morris Richman on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Seattle_Bridge_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
