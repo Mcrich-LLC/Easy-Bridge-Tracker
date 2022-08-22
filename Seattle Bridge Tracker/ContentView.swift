dd2fc7d1646bc9ed59ee3c047a37ddf5d7565794
Morris Richman <mcrich23@icloud.com>
2022-08-21 22:48:27 -0700

    refactored where navigation view is for a better experience

--- Seattle Bridge Tracker/ContentView.swift
+++ Seattle Bridge Tracker/ContentView.swift
@@ -13,89 +13,97 @@ import SwiftUIBackports
 struct ContentView: View {
     @StateObject var viewModel = ContentViewModel()
     var body: some View {
-        VStack {
-        if !NetworkMonitor.shared.isConnected {
-            Text("Please check your internet and try again.")
-        } else {
-            switch viewModel.status {
-            case .loading:
-                ProgressView()
-            case .failed(let error):
-                errors(error: error)
-            case .success:
-                List {
-                    Section {
-                        ForEach(viewModel.bridges.sorted(), id: \.self) { bridge in
-                            Button {
-                                SwiftUIAlert.show(title: "Open Bridge?", message: "Do you want to open \(bridge.name) in maps?", preferredStyle: .alert, actions: [UIAlertAction(title: "Yes", style: .default, handler: { _ in
-                                    UIApplication.shared.open(bridge.mapsUrl)
-                                }), UIAlertAction(title: "Cancel", style: .cancel)])
-                            } label: {
-                                HStack {
-                                    if #available(iOS 15, *) {
-                                        AsyncImage(url: bridge.imageUrl) { image in
-                                            image
-                                                .resizable()
-                                                .frame(width: 60, height: 60)
-                                                .aspectRatio(contentMode: .fit)
-                                        } placeholder: {
-                                            Image(systemName: "photo")
-                                                .resizable()
-                                                .frame(width: 60, height: 60)
-                                                .aspectRatio(contentMode: .fit)
-                                                .foregroundColor(.gray)
-                                        }
-                                    } else {
-                                        URLImage(bridge.imageUrl) { image in
-                                            image
-                                                .resizable()
-                                                .frame(width: 60, height: 60)
-                                                .aspectRatio(contentMode: .fit)
-                                                .clipped()
-                                        }
-                                    }
-                                    HStack(alignment: .center) {
-                                        VStack(alignment: .leading) {
-                                            Text(bridge.name)
-                                                .foregroundColor(Color(.label))
-                                            Text(bridge.address)
-                                                .font(SwiftUI.Font.caption)
-                                                .foregroundColor(.gray)
-                                        }
-                                        Spacer()
-                                        if bridge.status == .up {
-                                            Text("Up")
-                                                .foregroundColor(.red)
-                                        } else if bridge.status == .maintenance {
-                                            Text("Under Maintenance")
-                                                .foregroundColor(.yellow)
-                                        } else if bridge.status == .unknown {
-                                            Text("Unknown")
-                                                .foregroundColor(.yellow)
+        NavigationView {
+            VStack {
+            if !NetworkMonitor.shared.isConnected {
+                Text("Please check your internet and try again.")
+            } else {
+                switch viewModel.status {
+                case .loading:
+                    ProgressView()
+                case .failed(let error):
+                    errors(error: error)
+                case .success:
+                    List {
+                        Section {
+                            ForEach(viewModel.bridges.sorted(), id: \.self) { bridge in
+                                Button {
+                                    SwiftUIAlert.show(title: "Open Bridge?", message: "Do you want to open \(bridge.name) in maps?", preferredStyle: .alert, actions: [UIAlertAction(title: "Yes", style: .default, handler: { _ in
+                                        UIApplication.shared.open(bridge.mapsUrl)
+                                    }), UIAlertAction(title: "Cancel", style: .cancel)])
+                                } label: {
+                                    HStack {
+                                        if #available(iOS 15, *) {
+                                            AsyncImage(url: bridge.imageUrl) { image in
+                                                image
+                                                    .resizable()
+                                                    .frame(width: 60, height: 60)
+                                                    .aspectRatio(contentMode: .fit)
+                                            } placeholder: {
+                                                Image(systemName: "photo")
+                                                    .resizable()
+                                                    .frame(width: 60, height: 60)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .foregroundColor(.gray)
+                                            }
                                         } else {
-                                            Text("Down")
-                                                .foregroundColor(.green)
+                                            URLImage(bridge.imageUrl) { image in
+                                                image
+                                                    .resizable()
+                                                    .frame(width: 60, height: 60)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .clipped()
+                                            }
+                                        }
+                                        HStack(alignment: .center) {
+                                            VStack(alignment: .leading) {
+                                                Text(bridge.name)
+                                                    .foregroundColor(Color(.label))
+                                                Text(bridge.address)
+                                                    .font(SwiftUI.Font.caption)
+                                                    .foregroundColor(.gray)
+                                            }
+                                            Spacer()
+                                            if bridge.status == .up {
+                                                Text("Up")
+                                                    .foregroundColor(.red)
+                                            } else if bridge.status == .maintenance {
+                                                Text("Under Maintenance")
+                                                    .foregroundColor(.yellow)
+                                            } else if bridge.status == .unknown {
+                                                Text("Unknown")
+                                                    .foregroundColor(.yellow)
+                                            } else {
+                                                Text("Down")
+                                                    .foregroundColor(.green)
+                                            }
                                         }
                                     }
                                 }
                             }
+                        } header: {
+                            Text("Seattle")
                         }
-                    } header: {
-                        Text("Seattle")
+                    }
+                        .backport.refreshable(action: {
+                            await viewModel.fetchData()
+                        })
+                        .tag(0)
+                        .listStyle(GroupedListStyle())
                     }
                 }
-                    .backport.refreshable(action: {
-                        await viewModel.fetchData()
-                    })
-                    .tag(0)
-                    .listStyle(GroupedListStyle())
-                }
+            }
+            .onAppear {
+                viewModel.fetchData()
+            }
+        .navigationBarTitle("Bridges", displayMode: .large)
+        }
+        .navigationViewStyle(.stack)
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                BannerAds()
             }
         }
-        .onAppear {
-            viewModel.fetchData()
-        }
-        .navigationBarTitle("Bridges", displayMode: .large)
     }
     
     func errors(error: String) -> some View {
