910a30efa2e36f681e8cf15e5d75d8ffcda68b3c
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2022-12-07 11:13:43 -0800

    Added removal for old notifications of a bridge

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -17,21 +17,40 @@ class NotificationService: UNNotificationServiceExtension {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         guard let bestAttemptContent = bestAttemptContent else { return }
-        if let RawInterruptionLevel = request.content.userInfo["interruption_level"] as? String {
-            let interruptionLevel = UInt(RawInterruptionLevel) ?? 1
-            print("interruptionLevel = \(interruptionLevel)")
-            if #available(iOSApplicationExtension 15.0, *) {
-                bestAttemptContent.interruptionLevel = .init(rawValue: interruptionLevel) ?? .active
+        func complete() {
+            if let RawInterruptionLevel = request.content.userInfo["interruption_level"] as? String {
+                let interruptionLevel = UInt(RawInterruptionLevel) ?? 1
+                print("interruptionLevel = \(interruptionLevel)")
+                if #available(iOSApplicationExtension 15.0, *) {
+                    bestAttemptContent.interruptionLevel = .init(rawValue: interruptionLevel) ?? .active
+                } else {
+                    // Fallback on earlier versions
+                }
             } else {
-                // Fallback on earlier versions
+                print("interruption_level = nil")
             }
-        } else {
-            print("interruption_level = nil")
+            
+            FIRMessagingExtensionHelper().populateNotificationContent(
+                bestAttemptContent,
+                withContentHandler: contentHandler)
         }
-        
-        FIRMessagingExtensionHelper().populateNotificationContent(
-            bestAttemptContent,
-            withContentHandler: contentHandler)
+        UNUserNotificationCenter.current().getDeliveredNotifications { deliveredNotifications in
+            if deliveredNotifications.isEmpty {
+                complete()
+            } else {
+                let filteredNotifications = deliveredNotifications.filter { getNotification in
+                    getNotification.request.content.body.removeBridgeStatus() == bestAttemptContent.body.removeBridgeStatus()
+                }
+                for notification in filteredNotifications {
+                    UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [notification.request.identifier])
+                    if notification == filteredNotifications.last {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
+                            complete()
+                        }
+                    }
+                }
+            }
+        }
     }
     
     override func serviceExtensionTimeWillExpire() {
@@ -42,4 +61,17 @@ class NotificationService: UNNotificationServiceExtension {
         }
     }
 
+}
+extension String {
+    
+    func slice(from: String, to: String) -> String? {
+        return (range(of: from)?.upperBound).flatMap { substringFrom in
+            (range(of: to, range: substringFrom..<endIndex)?.lowerBound).map { substringTo in
+                String(self[substringFrom..<substringTo])
+            }
+        }
+    }
+    func removeBridgeStatus() -> String {
+        return self.replacingOccurrences(of: "up", with: "").replacingOccurrences(of: "down", with: "").replacingOccurrences(of: "unknown", with: "")
+    }
 }
