37af22cc52af766813f29fbaae4ec26cff2330cb
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-03-26 21:15:44 -0700

    Added preferences handling to notification service

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -14,27 +14,58 @@ class NotificationService: UNNotificationServiceExtension {
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         guard let bestAttemptContent = bestAttemptContent else { return }
-        func complete() {
-            bestAttemptContent.categoryIdentifier = request.content.title
-            if let RawInterruptionLevel = request.content.userInfo["interruption_level"] as? String {
-                let interruptionLevel = UInt(RawInterruptionLevel) ?? 1
-                print("interruptionLevel = \(interruptionLevel)")
-                if #available(iOSApplicationExtension 15.0, *) {
-                    bestAttemptContent.interruptionLevel = .init(rawValue: interruptionLevel) ?? .active
+        
+        do {
+            enum throwError: Error {
+                case fileDoesNotExist
+                case preferenceDoesNotExist
+            }
+            if let bundlePath = Bundle.main.url(forResource: "NotificationPreferences", withExtension: "json") {
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                let preferencesArray = try jsonDecoder.decode([NotificationPreferences].self, from: Data(contentsOf: bundlePath))
+                
+                if let day = Day.currentDay(),
+                   let bridgeId = request.content.userInfo["bridge_id"] as? String,
+                   let preferences = preferences.first(where: { pref in
+                       pref.days.contains(day) && currentTimeIsBetween(startTime: pref.startTime, endTime: pref.endTime) && pref.bridgeIds.contains(bridgeId)
+                   }) {
+                    removeOldNotifications {
+                        pushNotification(preferences: preferences)
+                    }
                 } else {
-                    // Fallback on earlier versions
+                    throw throwError.preferenceDoesNotExist
                 }
             } else {
-                print("interruption_level = nil")
+                throw throwError.fileDoesNotExist
             }
-            
-            FIRMessagingExtensionHelper().populateNotificationContent(
-                bestAttemptContent,
-                withContentHandler: contentHandler)
+        } catch {
+//            removeOldNotifications {
+//                pushNotification()
+//            }
         }
+    }
+    
+    func pushNotification(preferences: NotificationPreferences) {
+        bestAttemptContent.categoryIdentifier = request.content.title
+        if #available(iOSApplicationExtension 15.0, *) {
+            switch preferences.notificationPriority {
+            case .timeSensitive: bestAttemptContent.interruptionLevel = .timeSensitive
+            case .normal: bestAttemptContent.interruptionLevel = .active
+            case .silent: bestAttemptContent.interruptionLevel = .passive
+            }
+        }
+        
+        FIRMessagingExtensionHelper().populateNotificationContent(
+            bestAttemptContent,
+            withContentHandler: contentHandler)
+    }
+    
+    func removeOldNotifications(then completion: @escaping () -> Void) {
         UNUserNotificationCenter.current().getDeliveredNotifications { deliveredNotifications in
             if deliveredNotifications.isEmpty {
                 complete()
@@ -46,12 +77,20 @@ class NotificationService: UNNotificationServiceExtension {
                     UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [notification.request.identifier])
                     if notification == filteredNotifications.last {
                         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
-                            complete()
+                            completion()
                         }
                     }
                 }
             }
         }
+    }
+    
+    func currentTimeIsBetween(startTime: String, endTime: String) -> Bool {
+        guard let start = Formatter.today.date(from: startTime),
+              let end = Formatter.today.date(from: endTime) else {
+            return false
+        }
+        return DateInterval(start: start, end: end).contains(Date())
     }
     
     override func serviceExtensionTimeWillExpire() {
@@ -75,4 +114,59 @@ extension String {
     func removeBridgeStatus() -> String {
         return self.replacingOccurrences(of: "up", with: "").replacingOccurrences(of: "down", with: "").replacingOccurrences(of: "unknown", with: "")
     }
+}
+
+struct NotificationPreferences: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let days: [Day]
+    let startTime: String
+    let endTime: String
+    let notificationPriority: NotificationPriority
+    let bridgeIds: [String]
+    
+    init(days: [Day], startTime: String, endTime: String, notificationPriority: NotificationPriority, bridgeIds: [String]) {
+        self.days = days
+        self.startTime = startTime
+        self.endTime = endTime
+        self.notificationPriority = notificationPriority
+        self.bridgeIds = bridgeIds
+    }
+}
+
+enum NotificationPriority: String, CaseIterable {
+    case timeSensitive = "time sensitive"
+    case normal = "normal"
+    case silent = "silent"
+}
+
+enum Day: String, CaseIterable, Codable {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+    
+    static func currentDay() -> Self? {
+        Self(rawValue: Date().dayOfWeek())
+    }
+}
+
+extension Date {
+    func dayOfWeek() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self).lowercased()
+    }
+}
+
+extension Formatter {
+    static let today: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .init(identifier: "en_US_POSIX")
+        dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
+        dateFormatter.dateFormat = "hh:mm a"
+        return dateFormatter
+    }()
 }
