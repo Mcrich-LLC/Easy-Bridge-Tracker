15e671cd0b37d15e39a63d7f5332478b5d447153
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-03-30 19:30:03 -0700

    Added pwa capability

---
+++ firebase/Website/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'bridges-cache-v1';
+const urlsToCache = [
+  '/images/Rounded Icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
