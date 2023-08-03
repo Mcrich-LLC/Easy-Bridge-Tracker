7ea9f6f404f278c33a346d468add927f781a58da
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-08-03 13:49:39 -0700

    Fixed time picker setting too quickly

--- Seattle Bridge Tracker/Notification Preferences/Body Components/NotificationPreferencesTimePicker.swift
+++ Seattle Bridge Tracker/Notification Preferences/Body Components/NotificationPreferencesTimePicker.swift
@@ -10,44 +10,49 @@ import Mcrich23_Toolkit
 
 struct NotificationPreferencesTimePicker: View {
     @Binding var preference: NotificationPreferences
+    @State var startTime = Date()
+    @State var endTime = Date()
     @ObservedObject var preferencesModel = NotificationPreferencesModel.shared
     @ObservedObject var contentViewModel = ContentViewModel.shared
+    
+    init(preference: Binding<NotificationPreferences>) {
+        self._preference = preference
+        self._startTime = State(initialValue: formattedDate(preferencesModel.preferencesArray.first(where: { $0.id == preference.wrappedValue.id })?.startTime))
+        self._endTime = State(initialValue: formattedDate(preferencesModel.preferencesArray.first(where: { $0.id == preference.wrappedValue.id })?.endTime))
+    }
+    
+    func formattedDate(_ date: String?) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .init(identifier: "en_US_POSIX")
+        dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
+        dateFormatter.dateFormat = "hh:mm a"
+        return dateFormatter.date(from: date ?? "8:00 AM")!
+    }
+    
+    func formattedString(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .init(identifier: "en_US_POSIX")
+        dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
+        dateFormatter.dateFormat = "hh:mm a"
+        return dateFormatter.string(from: date)
+    }
+    
     var body: some View {
-        Toggle("All Day", isOn: Binding(get: {
-            preference.isAllDay
-        }, set: { isAllDay in
-            self.preference.isAllDay = isAllDay
-        }))
+        Toggle("All Day", isOn: $preference.isAllDay)
         if !preference.isAllDay {
             VStack {
-                DatePicker("Start Time: ", selection: Binding(get: {
-                    let startTime = preferencesModel.preferencesArray.first(where: { $0.id == preference.id })?.startTime
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.locale = .init(identifier: "en_US_POSIX")
-                    dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
-                    dateFormatter.dateFormat = "hh:mm a"
-                    return dateFormatter.date(from: startTime ?? "8:00 AM")!
-                }, set: { newValue in
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.locale = .init(identifier: "en_US_POSIX")
-                    dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
-                    dateFormatter.dateFormat = "hh:mm a"
-                    self.preference.startTime = dateFormatter.string(from: newValue)
-                }), displayedComponents: [.hourAndMinute])
-                DatePicker("End Time: ", selection: Binding(get: {
-                    let endTime = preferencesModel.preferencesArray.first(where: { $0.id == preference.id })?.endTime
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.locale = .init(identifier: "en_US_POSIX")
-                    dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
-                    dateFormatter.dateFormat = "hh:mm a"
-                    return dateFormatter.date(from: endTime ?? "8:00 AM")!
-                }, set: { newValue in
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.locale = .init(identifier: "en_US_POSIX")
-                    dateFormatter.defaultDate = Calendar.current.startOfDay(for: Date())
-                    dateFormatter.dateFormat = "hh:mm a"
-                    self.preference.endTime = dateFormatter.string(from: newValue)
-                }), displayedComponents: [.hourAndMinute])
+                DatePicker("Start Time: ", selection: $startTime, displayedComponents: [.hourAndMinute])
+                DatePicker("End Time: ", selection: $endTime, displayedComponents: [.hourAndMinute])
+            }
+            .onChange(of: startTime) { newValue in
+                DispatchQueue.main.asyncAfter(wallDeadline: .now() + .seconds(2)) {
+                    self.preference.startTime = formattedString(newValue)
+                }
+            }
+            .onChange(of: endTime) { newValue in
+                DispatchQueue.main.asyncAfter(wallDeadline: .now() + .seconds(2)) {
+                    self.preference.endTime = formattedString(newValue)
+                }
             }
         }
     }
